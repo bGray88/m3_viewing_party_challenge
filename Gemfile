fef495eebeaf2898f57db859748ce4d1e4fb56d0
Brandon Gray <brandonjoe88@hotmail.com>
2023-02-13 13:29:51 -0700

    feat: challenges-authentication

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.2'
+ruby '2.7.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.6'
@@ -43,6 +43,8 @@ group :development, :test do
     gem 'simplecov'
     gem 'launchy'
     gem 'rspec-rails'
+    gem 'faker'
+    gem 'factory_bot_rails'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,38 @@
-class UsersController <ApplicationController 
-    def new 
-        @user = User.new()
-    end 
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
-    def show 
-        @user = User.find(params[:id])
-    end 
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def create 
-        user = User.create(user_params)
-        if user.save
-            redirect_to user_path(user)
-        else  
-            flash[:error] = user.errors.full_messages.to_sentence
-            redirect_to register_path
-        end 
-    end 
+  def login_form; end
 
-    private 
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Sorry, your credentials are bad"
+      redirect_to login_path
+    end
+  end
 
-    def user_params 
-        params.require(:user).permit(:name, :email)
-    end 
-end 
+  def create
+    user = User.create(user_params)
+    if user.save && user_params[:password] == user_params[:password_confirmation]
+      redirect_to user_path(user)
+    else
+      flash[:error] = user.errors.full_messages.to_sentence
+      redirect_to register_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
